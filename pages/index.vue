458b5fc276bae7f2caf846bd3de972687429816d
leotuor <leotuor@hotmail.com>
2025-06-22 22:03:35 -0300

    guardando

---
+++ pages/index.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="background-color">
+    <v-row>
+      <v-col>
+        <v-card style="max-width: 500px; margin: auto; margin-bottom: 30vh; margin-top: 2vh;">
+          <h1 style="margin-left: 38%;">Trancity</h1>
+        </v-card>
+        <v-card elevation="10" style="max-width: 400px; margin: auto;">
+          <div>
+            <h1 class="text-center">Bem-vindo ao Transporte Urbano</h1>
+            <p class="text-center">Acesse o sistema para gerenciar suas rotas e veículos.</p>
+          </div>
+          <v-img src="/Transporte-Urbano.jpg" cover>
+            <v-form>
+                <v-text-field variant="solo" label="email" required class="mb-3" v-model="email"></v-text-field>
+                <v-text-field variant="solo" label="Senha" type="password" required class="mb-3" v-model="password"></v-text-field>
+                <div class="right-align">
+                  <v-btn color="success" class="mt-2" @click="login" style="margin-left: 38%;">Enviar</v-btn>
+                </div>
+              </v-form>
+          </v-img>
+        </v-card>
+      </v-col>
+    </v-row>
+  </div>
+</template>
+<script>
+export default {
+  data: () => {
+    return {
+      
+    }
+  },
+
+  methods: {
+    
+  },
+};
+</script>
+<style scoped>
+.background-color {
+  min-height: 100vh;
+  background: linear-gradient(0deg, #47a1f5 0%, #bde0fa 100%);
+  display: flex;
+  justify-content: center;
+}
+
+</style>
---
+++ pages/login.vue
@@ -0,0 +1,75 @@
+<template>
+  <div class="login-background">
+    <v-container>
+      <v-row justify="center">
+        <v-col cols="12" lg="3">
+          <v-card class="pa-4" max-width="500">
+            <v-card-title class="text-h5 text-center">Login</v-card-title>
+            <v-card-text>
+              <v-form>
+                <v-text-field label="email" required class="mb-3" v-model="email"></v-text-field>
+                <v-text-field label="Senha" type="password" required class="mb-3" v-model="password"></v-text-field>
+                <div class="right-align">
+                  <h3>Esqueceu sua senha?</h3>
+                  <v-btn color="primary" class="mt-2" @click="login">Login</v-btn>
+                </div>
+              </v-form>
+            </v-card-text>
+          </v-card>
+        </v-col>
+      </v-row>
+    </v-container>
+  </div>
+</template>
+
+<script>
+export default {
+  data: () => {
+    return {
+      email: '',
+      password: '',
+    }
+  },
+
+  methods: {
+    async login() {
+      try {
+        const response = await fetch('http://localhost:3333/usuario/login', { 
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify({
+            login: this.email,
+            password: this.password,
+          }),
+        });
+        const data = await response.json();
+
+        if (!response.ok) {
+          alert('Erro ao logar, tente novamente mais tarde.');
+          return;
+        }
+        console.log(data.token);
+      } catch (error) {
+        
+      }
+      
+    }
+  },
+};
+</script>
+<style scoped>
+.login-background {
+  min-height: 100vh;
+  background: linear-gradient(135deg, #47a1f5 0%, #bde0fa 100%);
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+.right-align {
+  display: flex;
+  flex-direction: column;
+  align-items: flex-end;
+}
+</style>
